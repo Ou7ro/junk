15ff988b3e4766b195719c3d280a1cb3215feab4
Ou7ro <qew.qweqwe.2016@mail.ru>
2025-02-23 11:25:39 +0500

    Some little changes

--- main.py
+++ main.py
@@ -2,28 +2,26 @@ import openpyxl as xl
 from random import randint
 from dict import malfunction
 
-__author__ = "Chepik Pavel"
-__version__ = "0.1"
-
-wb = xl.load_workbook('Утиль.xlsx', data_only=True)
-
-sheet = wb.active
-max_row = sheet.max_row
 
 def get_value(model):
     value = f'{malfunction[model][randint(0, 3)]}, {malfunction[model][randint(4, 7)]}'
     return value
 
 
-for i in range(2, max_row + 1):
-    model = sheet.cell(row=i, column=1).value 
-    if model in malfunction:
-        value = get_value(model)
-        sheet.cell(column=3, row=i, value=value)
-    else:
-        sheet.cell(column=3, row=i, value='Неустранимые загрязнения')
+def added_reasons(sheet, max_row):
+    for i in range(2, max_row + 1):
+        model = sheet.cell(row=i, column=1).value
+        if model in malfunction:
+            value = get_value(model)
+            sheet.cell(column=3, row=i, value=value)
+        else:
+            sheet.cell(column=3, row=i, value='Неустранимые загрязнения')
 
 
+wb = xl.load_workbook('Утиль.xlsx', data_only=True)
+sheet = wb.active
+max_row = sheet.max_row
+added_reasons(sheet, max_row)
 wb.save('Утиль.xlsx')
 wb.close()
 
